52f50e3cb537c7688c83e2d28632c42fcf49eb3d
John Doe <ddiiwaa1985@mail.ru>
2024-11-20 01:51:02 +0500

    SKYPRO_PYTHON_HOMEWORKS2

--- lesson_8/project_page.py
+++ lesson_8/project_page.py
@@ -11,13 +11,14 @@ class ProjectPage:
     def create_project(self, title="Тестовое питон", users=None):
         if users is None:
             users = {
-                "9cd8bd05-0883-4978-b576-5b90900a872e": "worker"
+                "ZWKQHXN-FZ645SB-PKAE82N-TPPN8Y8": "worker"
             }
         payload = {
             "title": title,
             "users": users
         }
-        response = requests.post(self.BASE_URL, json=payload, headers=self.HEADERS)
+        response = requests.post(
+            self.BASE_URL, json=payload, headers=self.HEADERS)
         response.raise_for_status()  # Выдает ошибку для статусов 4xx и 5xx
         return response.json()['id']
 
@@ -26,10 +27,11 @@ class ProjectPage:
         response.raise_for_status()
         return response.json()
 
-    def update_project(self, project_id, title="Тестовое питон измененное", users=None):
+    def update_project(
+            self, project_id, title="Тестовое питон измененное", users=None):
         if users is None:
             users = {
-                "9cd8bd05-0883-4978-b576-5b90900a872e": "worker"
+                "ZWKQHXN-FZ645SB-PKAE82N-TPPN8Y8": "worker"
             }
         update_url = f"{self.BASE_URL}/{project_id}"
         payload = {
@@ -42,17 +44,19 @@ class ProjectPage:
         return self.get_project_by_id(project_id)
 
     def get_project_by_id(self, project_id):
-        response = requests.get(f"{self.BASE_URL}/{project_id}", headers=self.HEADERS)
+        response = requests.get(
+            f"{self.BASE_URL}/{project_id}", headers=self.HEADERS)
         response.raise_for_status()
         return response.json()
 
     def create_project_without_title(self):
         payload = {
             "users": {
-                "9cd8bd05-0883-4978-b576-5b90900a872e": "worker"
+                "ZWKQHXN-FZ645SB-PKAE82N-TPPN8Y8": "worker"
             }
         }
-        response = requests.post(self.BASE_URL, json=payload, headers=self.HEADERS)
+        response = requests.post(
+            self.BASE_URL, json=payload, headers=self.HEADERS)
         return response.status_code
 
     def update_project_without_id(self):
@@ -63,41 +67,3 @@ class ProjectPage:
         }
         response = requests.put(update_url, json=payload, headers=self.HEADERS)
         return response.status_code
-
-
-# Примеры использования
-project_page = ProjectPage()
-
-
-def test_get_projects():
-    response = project_page.get_projects()
-    assert isinstance(response.get('content'), list)
-
-
-def test_create_project_increases_count():
-    initial_count = project_page.get_projects().get('paging', {}).get('count', 0)
-    project_page.create_project()
-    updated_count = project_page.get_projects().get('paging', {}).get('count', 0)
-    assert updated_count == initial_count + 1
-
-
-def test_update_project():
-    project_id = project_page.create_project()
-    response = project_page.update_project(project_id)
-    assert response['title'] == "Тестовое питон измененное"
-
-
-def test_get_project_by_id():
-    project_id = project_page.create_project()
-    response = project_page.get_project_by_id(project_id)
-    assert response['id'] == project_id
-
-
-def test_create_project_without_title():
-    status_code = project_page.create_project_without_title()
-    assert status_code == 400
-
-
-def test_update_project_without_id():
-    status_code = project_page.update_project_without_id()
-    assert status_code == 404
--- lesson_8/test_requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def test_simple_req():
-    resp = requests.get ('https://httpbin.org/basic-auth/user/pass')
-    
-    response_body = resp.json()
-
-    assert resp.status_code == 200
-    assert resp.headers["Content-Type"] == "appiication/json"
